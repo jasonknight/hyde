ddc4caefd34463432298d2f549e25d6cdb9f10cc
Jason Martin <jason@thebigrede.net>
2016-08-04 16:29:28 +0200

    Reorganizing code

--- layout.go
+++ layout.go
@@ -1,9 +1,20 @@
 package main
 func DefaultLayout() string {
     return `
-    <html>
+    <!DOCTYPE html>
+    <html lang="en">
         <head>
             <title>{{$title}}</title>
+            <meta name="description" content="{{$metadescription}}">
+            <meta charset="utf-8">
+            <meta name="author" content="{{$author}}">
+            <META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
+            <!--[if lt IE 9]>
+                <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            <![endif]-->
+            ript src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+            <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
         </head>
         <body>
             <h1>{{$title}}</h1>
